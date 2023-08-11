ec0b7d50eb15cf2f2f66514c48ee542d75afb991
Mārtiņš Sliede <martinssliede@gmail.com>
2023-08-11 19:01:49 +0300

    Cards added

--- Rick and Morty API/README.md
+++ Rick and Morty API/README.md
@@ -1,10 +1,13 @@
-# html_ts_boilerplate
-To start coding open your terminal and write
+# Rick and Morty API cards
+To view this project, open it using IDE (VS Code, IntelliJ...)
+
+Write this command in terminal
 ```
 npm i
 ```
 
-When all packages have been installed run:
+When all packages have been installed, run:
 ```
 npm start
 ```
+Open your browser with designated localhost address.
--- Rick and Morty API/src/scripts.ts
+++ Rick and Morty API/src/scripts.ts
@@ -1,50 +1,25 @@
 import axios from 'axios';
 
-const form = document.querySelector<HTMLFormElement>(".js-form");
-const posts = document.querySelector(".js-posts");
-const submitButton = document.querySelector<HTMLButtonElement>(".js-submit");
-
-type DataToSend = {[key: string]: unknown};
-type Post = {
-    author: string
-    id: number
-    title: string 
+type Character = {
+    name: string;
+    image: string;
 }
 
-const addPostsHTML = (post: Post) => {
-    const postHTML = `
-        <div>
-        <h1>${post.title}</h1>
-        <p>${post.author}</p>
-        </div>
-        <br><br>
-        `;
-}
+const characters = axios.get("https://rickandmortyapi.com/api/character");
+const wrapperElement = document.querySelector(".js-wrapper");
 
-axios.get("http://localhost:3004/posts").then((res) => {
-    console.log(res.data);
-    res.data.forEach((post: Post) => {
-        addPostsHTML(post)
-    });    
-})
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    submitButton.disabled = true;
-    const formData = new FormData(form);
-
-    const finalData: DataToSend = {};
-
-    for (const pair of formData.entries()) {
-        finalData[pair[0]] = pair[1];
-    }
+characters.then((response) => {
+    console.log("response", response.data.results);
     
-    axios.post("http://localhost:3004/posts", finalData).then((res) => {
-        const post: Post = res.data;
-        res.data.forEach((post: Post) => {
-            addPostsHTML(post)
-        });   
+
+    response.data.results.forEach((character: Character) => {        
+        const characterHTML = `
+        <div class="character">
+            <img src="${character.image}" class="character__image"/>
+            <h3 class="character__name">${character.name}</h3>
+        </div>
+        `;
+
+        wrapperElement.innerHTML += characterHTML; 
     });
-    form.reset();
-    submitButton.disabled = false;
-})
+});
